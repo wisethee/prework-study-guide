a0e03b849d92e12ec52981cbbb5a5573f0777e5d
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2022-10-10 09:00:58 +0100

    add js to project

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+const topics = ["HTML", "CSS", "Git", "JavaScript"];
+topics.forEach((topic) => console.log(topic));
+
+// if (topic === 'HTML') {
+//  console.log("Let's study HTML!");
+// } else if (topic === 'CSS') {
+//  console.log("Let's study CSS!");
+// } else if (topic === 'Git') {
+//  console.log("Let's study Git!");
+// } else if (topic === 'JavaScript') {
+//  console.log("Let's study JavaScript!");
+// } else {
+//  console.log('Please try again!');
+// }
